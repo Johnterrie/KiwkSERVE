dc548e83209ddbad347e033cedbedb614fb5a9d7
shadare japheth <japhethshadare@gmail.com>
2023-08-13 14:20:45 +0100

    profesional commit

---
+++ client/src/component/professionalSignUp/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import  Professional  from './Professional';
+
+function App() {
+  return (
+    <div className="App">
+       <Professional />
+    </div>
+  );
+}
+
+export default App;
